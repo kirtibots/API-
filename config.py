cb427321624d0f19c4f61f2f9081d447f620137f
tiwariji890 <pk4108644@gmail.com>
2026-07-13 21:40:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN_HERE")
 
 # 📢 आपके लॉग ग्रुप की ID (ग्रुप आईडी हमेशा माइनस '-' से शुरू होती है, जैसे: -100123456789)
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "0")) 
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1004463869572")) 
 
 # अपनी असली टेलीग्राम संख्यात्मक आईडी (Numeric ID) यहाँ डालें (उदा: 543216789)
 # सिर्फ इसी आईडी से /broadcast और /stats कमांड्स काम करेंगे
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789")) 
+OWNER_ID = int(os.getenv("OWNER_ID", "8315627692")) 
 
 # अपने अपडेट चैनल और सपोर्ट ग्रुप का यूजरनेम यहाँ डालें (आगे @ ज़रूर लगाएँ)
-UPDATE_CHANNEL = "@YourUpdateChannel"   # उदा: @marco_updates
-SUPPORT_GROUP = "@YourSupportGroup"    # उदा: @marco_support
+UPDATE_CHANNEL = "https://t.me/KRISHNA_BOTS"   # उदा: @marco_updates
+SUPPORT_GROUP = "https://t.me/+YcuguNBgrnVjZTI0"    # उदा: @marco_support
 
 # कोर डाउनलोडर API और डेटाबेस फाइल का नाम
 COBALT_API_URL = "https://api-dl.cgm.rs/api/json"
